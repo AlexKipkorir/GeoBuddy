a27302adcf4ab8a26a7b406d380e7a774c2a88e3
dylanlek <dylan.lekolool@gmail.com>
2025-03-14 15:35:12 +0300

    tracker class

---
+++ app/src/main/java/com/example/geobuddy/Tracker.kt
@@ -0,0 +1,11 @@
+package com.example.geobuddy
+
+data class Tracker(
+    val name: String,
+    val status: String,
+    val battery: String,
+    val latitude: Double,
+    val longitude: Double,
+    val iconRes: Int? = null
+
+)

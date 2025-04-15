88abced5e24aa7da53a97266f79e74d9ebc3c3de
ALEX NGENO <akipkorir210@gmail.com>
2025-04-15 12:08:53 +0300

    Remove file from tracking

---
+++ app/src/main/java/com/example/geobuddy/repositories/TrackerRepository.kt
@@ -0,0 +1,4 @@
+package com.example.geobuddy.repositories
+
+class TrackerRepository {
+}
---
+++ app/src/main/java/com/example/geobuddy/retrofit/PetTrackerRequest.kt
@@ -0,0 +1,14 @@
+package com.example.geobuddy
+
+data class PetTrackerRequest (
+    val trackerName: String,
+    val imei: String,
+    val batteryCapacity: Double = 100.0,
+    val latitude: Double = 0.0,
+    val longitude: Double = 0.0,
+    val status: String = "active",
+    val breed: String,
+    val age: Int,
+    val description: String,
+    val ownerName: String,
+)
